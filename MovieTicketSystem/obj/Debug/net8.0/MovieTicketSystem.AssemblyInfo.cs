bf12bb96d2042edae501408f2bdb33111c1b7484
trungtran <baotrung06092003@gmail.com>
2025-06-17 02:06:15 +0700

    chore: update some UI

--- MovieTicketSystem/obj/Debug/net8.0/MovieTicketSystem.AssemblyInfo.cs
+++ MovieTicketSystem/obj/Debug/net8.0/MovieTicketSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieTicketSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30393e37e52901330af678fbdbdfff8a0fa5940b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0f90b6c8d7dae5341d19587e76700977aea9ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieTicketSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieTicketSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
